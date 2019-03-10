256444854454e7b99460f889cda42f302951c143
robertramosastudillo <robertramosastudillo@gmail.com>
2019-03-09 22:18:42 -0500

    Axios & Ajax

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,22 @@
 <template lang="html">
-    <h1>Hola mundo</h1>
+    <h1>{{mensaje}}</h1>
 </template>
 
 <script>
+import axios from 'axios';
+import env from '../config/env';
 export default {
-    name: 'IndexPage'
+    name: 'IndexPage',
+    data(){
+        return{
+            mensaje: ''
+        }
+    },
+    created(){
+        axios.get(`${env.endpoint}/albums`).then(response=>{
+            this.mensaje = response.status;
+        })
+    }
 }
 </script>
 
